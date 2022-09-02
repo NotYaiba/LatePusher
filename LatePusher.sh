af1489fd3cef9f0ab92b5a39c3254a469c8ba956
NotYaiba <Elkarmimed@gmail.com>
2022-09-02 19:30:52 +0100

    ghadi

--- LatePusher.sh
+++ LatePusher.sh
@@ -3,10 +3,19 @@
 
 function screenIsLocked { [ "$(/usr/libexec/PlistBuddy -c "print :IOConsoleUsers:0:CGSSessionScreenIsLocked" /dev/stdin 2>/dev/null <<< "$(ioreg -n Root -d1 -a)")" = "true" ] && return 0 || return 1; }
 
-DIRECTORY="/goinfre/melkarmi/42Cursus-ft_transcendence"
+
 
 function pushToGit {
     cd "$DIRECTORY" ; git pull origin front-end  ; git add . ;  git commit -m "Late Pusher Push You are Welcome | $now" ; git push origin front-end
+}
+function isGitRepo {
+    git -C $DIRECTORY   rev-parse
+    if [ $? == 0 ]
+        then
+            return 1;
+        else
+            return 0;
+    fi
 }
 function LatePusher {
    
@@ -23,9 +32,17 @@ do
 done
 }
 
+
 if [ $# -eq 1 ] 
     then
-        echo $1
+        DIRECTORY=$1
     else
-        echo "invalid argument please pass only one argument "
+        echo "Pass the path of the directory that you wanna LatePush!"
+         
+    echo "shit"
+    if isGitRepo; then
+        LatePusher
+    else
+        echo "This is not a giit repo"
+    fi
 fi
